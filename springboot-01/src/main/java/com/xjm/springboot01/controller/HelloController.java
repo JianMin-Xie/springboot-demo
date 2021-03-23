8ee8880a7b16b52cbfebda5f1b3d063b1f2175e9
JianMin-Xie <854377742@qq.com>
2021-03-24 04:19:45 +0800

    1.hello world + 自定义banner

---
+++ springboot-01/src/main/java/com/xjm/springboot01/controller/HelloController.java
@@ -0,0 +1,16 @@
+package com.xjm.springboot01.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author Panda
+ * @create 2021-03-24 4:00
+ */
+@RestController
+public class HelloController {
+    @RequestMapping("/hello")
+    public String hello(){
+        return "hello,world444";
+    }
+}

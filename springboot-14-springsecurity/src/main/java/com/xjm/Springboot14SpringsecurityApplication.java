aaa8de1730dbb948755cd0718e6ec099fdaa4b3c
JianMin-Xie <854377742@qq.com>
2021-03-28 01:11:37 +0800

    12.集成SpringSecurity:认证和授权+权限控制和注销+记住我+定制登录页

---
+++ springboot-14-springsecurity/src/main/java/com/xjm/Springboot14SpringsecurityApplication.java
@@ -0,0 +1,13 @@
+package com.xjm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Springboot14SpringsecurityApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Springboot14SpringsecurityApplication.class, args);
+    }
+
+}
---
+++ springboot-14-springsecurity/src/test/java/com/xjm/Springboot14SpringsecurityApplicationTests.java
@@ -0,0 +1,13 @@
+package com.xjm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Springboot14SpringsecurityApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

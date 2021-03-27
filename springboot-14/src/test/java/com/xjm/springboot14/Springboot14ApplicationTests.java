611c8d84aedd6a35449062df04a1baebc4c78067
JianMin-Xie <854377742@qq.com>
2021-03-27 18:13:42 +0800

    11.富文本编辑器

---
+++ springboot-14/src/test/java/com/xjm/springboot14/Springboot14ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.xjm.springboot14;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Springboot14ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

a5097d715b1b4b5ee81be485cddcb83a0a82d3fb
JianMin-Xie <854377742@qq.com>
2021-03-29 00:25:41 +0800

    16.Shiro实现用户认证

--- springboot-16-shiro2/src/main/java/com/xjm/springboot16shiro2/config/UserRealm.java
+++ springboot-16-shiro2/src/main/java/com/xjm/springboot16shiro2/config/UserRealm.java
@@ -1,11 +1,15 @@
 package com.xjm.springboot16shiro2.config;
 
+import org.apache.shiro.SecurityUtils;
 import org.apache.shiro.authc.AuthenticationException;
 import org.apache.shiro.authc.AuthenticationInfo;
 import org.apache.shiro.authc.AuthenticationToken;
+import org.apache.shiro.authc.SimpleAuthenticationInfo;
+import org.apache.shiro.authc.UsernamePasswordToken;
 import org.apache.shiro.authz.AuthorizationInfo;
 import org.apache.shiro.realm.AuthorizingRealm;
 import org.apache.shiro.subject.PrincipalCollection;
+import org.apache.shiro.subject.Subject;
 
 /**
  * @author Panda
@@ -21,6 +25,16 @@ public class UserRealm extends AuthorizingRealm {
     @Override
     protected AuthenticationInfo doGetAuthenticationInfo(AuthenticationToken authenticationToken) throws AuthenticationException {
         System.out.println("执行了认证=>doGetAuthenticationInfo");
-        return null;
+
+        //用户名、密码  应该是数据库中取
+        String name = "root";
+        String password = "123456";
+
+        UsernamePasswordToken token = (UsernamePasswordToken)authenticationToken;
+        if(!token.getUsername().equals(name)){
+            return null;//抛出异常
+        }
+        //密码认证，shiro去做，不需要你做
+        return new SimpleAuthenticationInfo("",password,"");
     }
 }
